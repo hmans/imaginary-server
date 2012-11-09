340e8d70074644b4676ce65365dbcba48d0668b0
Hendrik Mans <hendrik@mans.de>
2012-11-09 19:59:09 +0100

    Add #serve route and method.

--- app/controllers/images_controller.rb
+++ app/controllers/images_controller.rb
@@ -1,4 +1,10 @@
 class ImagesController < InheritedResources::Base
   respond_to :json, :xml
   belongs_to :bucket
+
+  def serve
+    @bucket = Bucket.find(params[:bucket_id])
+    @image = @bucket.images.find_by_name!(params[:name])
+    render text: @image.image.data, content_type: 'image/jpeg'
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Imaginary::Application.routes.draw do
   resources :buckets, format: :json do
     resources :images, format: :json
+    get 'i/:name' => 'images#serve'
   end
 
   # root to: 'buckets#index'
