ea7a7d8a1f7a6f62ad6dd2eeae6c6a78f4aca6ed
Hendrik Mans <hendrik@mans.de>
2012-11-09 21:17:13 +0100

    Transform images. Yay!

--- app/controllers/images_controller.rb
+++ app/controllers/images_controller.rb
@@ -5,6 +5,37 @@ class ImagesController < InheritedResources::Base
   def serve
     @bucket = Bucket.find(params[:bucket_id])
     @image = @bucket.images.find_by_name!(params[:name])
-    render text: @image.image.data, content_type: 'image/jpeg'
+
+    # start with the source image
+    @job = @image.image
+
+    options = {}
+    if params[:options].present?
+      params[:options].split(',').each do |option|
+        key, value = option.split('_')
+        case key
+          when 'w' then options[:width] = value
+          when 'h' then options[:height] = value
+          when 'c' then options[:crop_mode] = value
+          else raise "Unrecognized option #{option}."
+        end
+      end
+    end
+
+    if options.any?
+      case (options.delete(:crop_mode) || :scale).to_sym
+        when :scale
+          @job = @job.process(:resize, "#{options[:width]}x#{options[:height]}!")
+        when :fit
+          @job = @job.process(:resize, "#{options[:width]}x#{options[:height]}")
+        when :fill
+          @job = @job.process(:resize_and_crop, options)
+        else
+          raise "Unrecognized crop mode."
+      end
+    end
+
+    # render new jpg
+    render text: @job.data, content_type: 'image/jpeg'
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Imaginary::Application.routes.draw do
   resources :buckets, format: :json do
     resources :images, format: :json
-    get 'i/:name' => 'images#serve'
+    get 'i/:name(/:options)' => 'images#serve'
   end
 
   # root to: 'buckets#index'
