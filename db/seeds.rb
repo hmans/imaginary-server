b371d37e559db05d8ffa8a5be2796b84b71d5d89
Hendrik Mans <hendrik@mans.de>
2012-11-10 19:32:24 +0100

    Changed seeds so images have meaningful names.

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,5 +7,5 @@
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
 bucket = Bucket.create!(name: 'test', secret: 'abcdefg')
-bucket.images.create!(image_url: 'http://3.bp.blogspot.com/-4kBvHvCJnOY/T-iKBteyTKI/AAAAAAAABLw/CwDU11DBqWc/s1600/kitten.jpeg')
-bucket.images.create!(image: File.new('./spec/files/hmans.jpg'))
+bucket.images.create!(image_url: 'http://3.bp.blogspot.com/-4kBvHvCJnOY/T-iKBteyTKI/AAAAAAAABLw/CwDU11DBqWc/s1600/kitten.jpeg', name: 'kitten')
+bucket.images.create!(image: File.new('./spec/files/hmans.jpg'), name: 'hmans')
