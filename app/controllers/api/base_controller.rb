587e244ecd8bda1a06a619d2a732b6d4bb561b3b
Hendrik Mans <hendrik@mans.de>
2012-11-10 19:08:32 +0100

    Moved API credentials into ENV variables.

--- app/controllers/api/base_controller.rb
+++ app/controllers/api/base_controller.rb
@@ -1,3 +1,3 @@
 class Api::BaseController < InheritedResources::Base
-  http_basic_authenticate_with :name => "zomg", :password => "zomg"
+  http_basic_authenticate_with :name => ENV['IMAGINARY_API_USERNAME'], :password => ENV['IMAGINARY_API_PASSWORD']
 end
