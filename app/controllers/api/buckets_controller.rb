bde3b26c8e6779139b429be504f2bc835198261e
Hendrik Mans <hendrik@mans.de>
2012-11-10 15:35:59 +0100

    Updated web UI.

--- app/controllers/api/buckets_controller.rb
+++ app/controllers/api/buckets_controller.rb
@@ -1,9 +1,3 @@
 class Api::BucketsController < Api::BaseController
   respond_to :json, :xml
-
-  def show
-    show! do |format|
-      format.html { redirect_to api_bucket_images_path(@bucket) }
-    end
-  end
 end
