1c70fb47af939d062ec67b2f37d7aa99a3db9152
Hendrik Mans <hendrik@mans.de>
2013-01-20 03:26:27 +0100

    Update Resourcery

--- app/controllers/images_controller.rb
+++ app/controllers/images_controller.rb
@@ -1,10 +1,17 @@
 class ImagesController < ResourceController
   def resource_base
-    @bucket = Bucket.where(name: params[:bucket_id]).first!
-    @bucket.images
+    bucket.images
   end
 
   def resource_key
     :name
+  end
+
+  def bucket
+    @bucket ||= Bucket.where(name: params[:bucket_id]).first!
+  end
+
+  def resource_parents
+    [bucket]
   end
 end
